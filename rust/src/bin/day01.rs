2f806fa4c597aba1cb93fdaea716182af4c6679c
Thomas Fisker Christensen <thomas@chrstnsn.dk>
2020-04-13 14:42:58 +0200

    part 2

--- rust/src/bin/day01.rs
+++ rust/src/bin/day01.rs
@@ -1,6 +1,7 @@
-use rust::day01::{input, part_1};
+use rust::day01::{input, part_1, part_2};
 
 fn main() {
     let input = input();
-    println!("solution: {:?}", part_1(input));
+    println!("part1 solution: {:?}", part_1::solution(&input));
+    println!("part2 solution: {:?}", part_2::solution(&input));
 }
--- rust/src/day01.rs
+++ rust/src/day01.rs
@@ -8,25 +8,71 @@ pub fn calc_fuel(mass: u32) -> u32 {
     mass / 3 - 2
 }
 
-macro_rules! fuel_tests {
-    ($($name:ident: $value:expr,)*) => {
-    $(
-        #[test]
-        fn $name() {
-            let (input, expected) = $value;
-            assert_eq!(expected, calc_fuel(input));
+pub mod part_1 {
+    macro_rules! fuel_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                assert_eq!(expected, super::calc_fuel(input));
+            }
+        )*
         }
-    )*
+    }
+
+    fuel_tests! {
+        ex1: (12,2),
+        ex2: (14,2),
+        ex3: (1969, 654),
+        ex4: (100756, 33583),
+    }
+
+    pub fn solution(masses: &Vec<u32>) -> u32 {
+        masses.iter().map(|&m| super::calc_fuel(m)).sum()
     }
 }
 
-fuel_tests! {
-    ex1: (12,2),
-    ex2: (14,2),
-    ex3: (1969, 654),
-    ex4: (100756, 33583),
-}
+pub mod part_2 {
+    fn rec_fuel(mass: u32) -> u32 {
+        let mut result: u32 = 0;
+        let mut fuel: u32 = mass;
 
-pub fn part_1(masses: Vec<u32>) -> u32 {
-    masses.iter().map(|&m| calc_fuel(m)).sum()
+        loop {
+            fuel = fuel / 3;
+
+            if fuel < 2 {
+                break;
+            }
+
+            fuel -= 2;
+
+            result += fuel;
+        }
+
+        result
+    }
+
+    macro_rules! rec_fuel_tests {
+        ($($name:ident: $value:expr,)*) => {
+        $(
+            #[test]
+            fn $name() {
+                let (input, expected) = $value;
+                assert_eq!(expected, rec_fuel(input));
+            }
+        )*
+        }
+    }
+
+    rec_fuel_tests! {
+        ex1: (12,2),
+        ex2: (14,2),
+        ex3: (1969, 966),
+        ex4: (100756, 50346),
+    }
+
+    pub fn solution(masses: &Vec<u32>) -> u32 {
+        masses.iter().map(|&m| rec_fuel(m)).sum()
+    }
 }
